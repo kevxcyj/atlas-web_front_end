e6d6481f779725a822b103d76a1efe445867d579
kevxcyj <kevcyjunior@gmail.com>
2024-11-20 11:25:17 -0600

    Added number 2

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,13 @@
+function welcomeMessage(fullName) {
+    return function() {
+      alert("Welcome " + fullName);
+    };
+  }
+  
+  let guillaume = welcomeMessage("Guillaume");
+  let alex = welcomeMessage("Alex");
+  let fred = welcomeMessage("Fred");
+
+  guillaume();
+  alex();
+  fred();
