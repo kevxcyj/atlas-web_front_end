a38e0e7d14221bfa20f93c26ea7861d32185c046
kevxcyj <kevcyjunior@gmail.com>
2024-11-20 12:57:46 -0600

    Added nuimber 9

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,23 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    for (let i = 2; i <= 100; i++) {
+      let isPrime = true;
+
+      for (let j = 2; j * j <= i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) 
+        count++;
+    }
+    return count;
+  }
+  
+  let startTime = performance.now();
+  let count = countPrimeNumbers();
+  let endTime = performance.now();
+  
+  console.log(`Execution time of printing countPrimeNumbers was ${endTime - startTime} milliseconds.`);
